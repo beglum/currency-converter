ca383fc3e85c39bf30e3497347e3644e3897a6e6
Henrik Kolotov <tobeglum@gmail.com>
2021-11-14 23:09:26 +0300

    Merge branch 'main' of https://github.com/beglum/currency-converter

--- composables/useConverter.js
+++ composables/useConverter.js
@@ -5,6 +5,9 @@ export default function () {
   const { fetchActualRate, fetchRateHistory } = useApi();
   const { multiplyCurrencyQuantity, divideCurrencyQuantity } = useCalculation();
 
+  /**
+   * Параметры конвертера
+   */
   const chosenCurrencies = reactive([
     {
       currencyName: 'USD',
@@ -15,51 +18,6 @@ export default function () {
       quantity: 1,
     },
   ]);
-
-  const actualRate = ref({});
-  const currencyList = computed(() => Object.keys(actualRate.value));
-  const updateActualRate = function (currency) {
-    const result = fetchActualRate(currency);
-
-    result.then((data) => {
-      actualRate.value = data;
-      updateCurrency(0, 'quantity', chosenCurrencies[0].quantity);
-    });
-  };
-
-  const rateHistory = ref({});
-  const updateRateHistory = function (currency) {
-    const result = fetchRateHistory(currency);
-
-    result.then((data) => {
-      rateHistory.value = data;
-    });
-  };
-
-  const updateCurrenciesData = function () {
-    const currencyName = chosenCurrencies[0].currencyName;
-    updateActualRate(currencyName);
-    updateRateHistory(currencyName);
-  };
-
-  const switchCurrency = function () {
-    const baseCurrency = chosenCurrencies[0].currencyName;
-    const currencyName = chosenCurrencies[1].currencyName;
-    chosenCurrencies[0].currencyName = currencyName;
-    chosenCurrencies[1].currencyName = baseCurrency;
-
-    updateCurrenciesData();
-  };
-
-  const updateCurrency = function (index, param, value) {
-    chosenCurrencies[index][param] = value;
-    updateDependentCurrencies(param, index);
-  };
-
-  const silentUpdateCurrency = function (index, param, value) {
-    chosenCurrencies[index][param] = value;
-  };
-
   const converterParams = computed(() => {
     return chosenCurrencies.map((currency) => {
       return {
@@ -68,6 +26,21 @@ export default function () {
       };
     });
   });
+  const updateCurrency = function (index, param, value) {
+    chosenCurrencies[index][param] = value;
+    updateDependentCurrencies(param, index);
+  };
+  const silentUpdateCurrency = function (index, param, value) {
+    chosenCurrencies[index][param] = value;
+  };
+  const switchCurrency = function () {
+    const baseCurrency = chosenCurrencies[0].currencyName;
+    const currencyName = chosenCurrencies[1].currencyName;
+    chosenCurrencies[0].currencyName = currencyName;
+    chosenCurrencies[1].currencyName = baseCurrency;
+
+    updateCurrenciesData();
+  };
 
   const updateDependentCurrencies = function (paramName, changedParamIndex) {
     const baseCurrency = converterParams.value[0];
@@ -110,6 +83,35 @@ export default function () {
         );
       }
     }
+  };
+
+  /**
+   * Актуальные данные курса валют
+   */
+  const actualRate = ref({});
+  const currencyList = computed(() => Object.keys(actualRate.value));
+  const updateActualRate = function (currency) {
+    const result = fetchActualRate(currency);
+
+    result.then((data) => {
+      actualRate.value = data;
+      updateCurrency(0, 'quantity', chosenCurrencies[0].quantity);
+    });
+  };
+
+  const rateHistory = ref({});
+  const updateRateHistory = function (currency) {
+    const result = fetchRateHistory(currency);
+
+    result.then((data) => {
+      rateHistory.value = data;
+    });
+  };
+
+  const updateCurrenciesData = function () {
+    const currencyName = chosenCurrencies[0].currencyName;
+    updateActualRate(currencyName);
+    updateRateHistory(currencyName);
   };
 
   return {
